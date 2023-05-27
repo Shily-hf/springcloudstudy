5aa0e84b8539068f7f8090ca0e93e06b5a72c4d6
Shily-zhang <1240323810@qq.com>
2023-05-27 15:22:48 +0800

    Consul学习

---
+++ cloud-consumerconsul-order80/src/main/java/edu/shily/springcloud/config/ApplicationContextBean.java
@@ -0,0 +1,21 @@
+package edu.shily.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Shily-zhang
+ * @Description
+ */
+@Configuration
+public class ApplicationContextBean
+{
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/edu/shily/springcloud/PaymentMain8006.java
@@ -0,0 +1,15 @@
+package edu.shily.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shily-zhang
+ * @Description
+ */
+@SpringBootApplication
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
